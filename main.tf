c2542b0570a3357a1de1373f4c8b928ad9376935
_clsyan <yan.victor@hoobox.one>
2023-04-15 22:53:38 -0300

    initial commit

---
+++ main.tf
@@ -0,0 +1,65 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  token = var.do_token
+}
+
+# Create a new Web Droplet in the nyc2 region
+resource "digitalocean_droplet" "jenkins" {
+  image    = "ubuntu-18-04-x64"
+  name     = "jenkins"
+  region   = var.region
+  size     = "s-1vcpu-1gb"
+  ssh_keys = [data.digitalocean_ssh_key.ssh_key.id]
+}
+
+data "digitalocean_ssh_key" "ssh_key" {
+  name = var.ssh_key
+}
+
+resource "digitalocean_kubernetes_cluster" "k8s" {
+  name   = "k8s"
+  region = var.region
+  # Grab the latest version slug from `doctl kubernetes options versions`
+  version = "1.24.12-do.0"
+
+  node_pool {
+    name       = "worker-pool"
+    size       = "s-2vcpu-2gb"
+    node_count = 1
+
+    taint {
+      key    = "workloadKind"
+      value  = "database"
+      effect = "NoSchedule"
+    }
+  }
+}
+
+resource "local_file" "foo" {
+  content  = digitalocean_kubernetes_cluster.k8s.kube_config.0.raw_config
+  filename = "kube_config.yaml"
+}
+
+variable "do_token" {
+  default = ""
+}
+
+variable "ssh_key" {
+  default = ""
+}
+
+variable "region" {
+  default = ""
+}
+
+output "jenkins_ip" {
+  value = digitalocean_droplet.jenkins.ipv4_address
+}
